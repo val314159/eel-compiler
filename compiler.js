339fd17538c59f2893ecf464188b34aed808b649
Joel Ward <jmward@gmail.com>
2016-04-02 18:59:38 -0700

    make it work

--- compiler.js
+++ compiler.js
@@ -1,48 +1,36 @@
-function atom(x){return(typeof(x)!=='object'||typeof(x.length)!=='number')}
+// compiler.js
 function Compiler(){}
-var C=module.exports=Compiler;
-C.compileSymbol=function(x){return String(x).slice(7,-1);}
-C.compileString=function(x){return JSON.stringify(x);}
-C.compileAtom=function(x){
-    if(typeof(x)==='symbol')	return C.compileSymbol(x);
-    if(typeof(x)==='string')	return C.compileString(x);
-    return ""+x;
-};
-C.compileList=function(x){
-    var fn=x[0];
-    if(typeof(fn)!=='symbol') print("MASSIVE ERROR THE CAR ISNT A SYMBOL");
-    fn = C.compileSymbol(fn);
-    var args=[];
-    for(var n=1;n<x.length;n++){
-	var v=x[n];
-	args.push( C.compileExpr(v) );
-    }
-    return fn+"("+args.join(",")+")";
-};
-C.compileExpr=function(x){
-    return atom(x)?C.compileAtom(x):C.compileList(x)};
-C.compileBlock=function(body){
-    var arr=[];
-    for(var n=0;n<body.length;n++){
-	var v=body[n];
-	arr.push( C.compileExpr(v) );
-    }
-    return "{"+arr.join(";")+"}";
-};
-C.compileParms=function(args){
-    var arr=[];
-    for(var n=0;n<args.length;n++){
-	var v=args[n];
-	arr.push(typeof(v)==='symbol'?C.compileSymbol(v):v);    
-    }
-    return "("+arr.join(',')+")";
-};
-C.compileFunc=function(name,args,body){
-    name = name || "";
-    var a = C.compileParms(args);
-    var b = C.compileBlock(body);
-    return "(function "+name+a+b+")";
-};
-C.compileSetq=function(name,value){
-    return ""+C.compileAtom(name)+"="+C.compileExpr(value)+";";
-};
+var C=Compiler;
+C._Macros={
+    'if': function(a,b,c){return("("+C.Expr(a)+")?("+C.Expr(b)+"):("+C.Expr(c)+")");},
+    'progn': function(){return C.Block([].slice.apply(arguments,[0]))}};
+C.nul=function(x){return x.length===0};
+C.atom=function(x){return typeof(x)!=='object'||typeof(x.length)!=='number'||x.length===0};
+C.Symbol=function(x){return x.toString().slice(7,-1);}
+C.String=function(x){return JSON.stringify(x);}
+C.Expr=function(x){return C.atom(x)?C.Atom(x):C.List(x)};
+C.Block=function(body){return "{"+body.map(C.Expr).join(";")+"}";};
+C.Setq=function(name,value){return ""+C.Atom(name)+"="+C.Expr(value)+";";};
+C.SoS=function(v){return typeof(v)==='symbol'?C.Symbol(v):v;};
+C.Parms=function(args){return "("+args.map(C.SoS).join(',')+")"};
+C.Atom=function(x){ return(C.nul(x)?"nil":
+			   typeof(x)==='symbol'?C.Symbol(x):
+			   typeof(x)==='string'?C.String(x):""+x)};
+C.List=function(x){
+    var mac,fn=x[0];
+    if(typeof(fn)==='symbol') fn=C.Symbol(fn)
+    if(mac=C.atom(fn) && C._Macros[fn])return mac.apply(null,x.slice(1))
+    else if(C.Symbol(fn[0])==='lambda')	fn=C.Func.apply(null,fn)
+    else if(typeof(fn)!=='string')
+	print("MASSIVE ERROR THE CAR ISNT A SYMBOL/LAMBDA",fn, typeof(fn));
+    return "_="+C.SoS(fn)+"("+x.slice(1).map(C.Expr).join(",")+")"}
+C.File=function(body,useReturn){
+    return "{"+body.map(function(v,n,a){
+	return((n==a.length-1&&useReturn===true)?"return ":"")+C.Expr(v);
+    }).join(";")+"}";};
+C.Func=function(name,args){
+    var body=Array.prototype.slice.apply(arguments,[2]);
+    name=name||"";
+    if(C.SoS(name)=='lambda')name="";
+    return "(function "+name+C.Parms(args)+C.File(body,true)+")";};
+try{module.exports=C;}catch(e){}
--- compiler_test.js
+++ compiler_test.js
@@ -1,16 +1,16 @@
 print=console.log.bind(console);
 var C=require('./compiler.js');
-print(C.compileExpr(Symbol("XX1")));
-print(C.compileExpr("XX2"));
-print(C.compileExpr(112));
-print(C.compileExpr([Symbol("XXF")]));
-print(C.compileExpr([Symbol("XXF"),1,2,3]));
-print(C.compileExpr([Symbol("XX"),1,2,"xx","yy",Symbol("wut")]));
-print(C.compileBlock([[Symbol("XX1"),1,"xxy"],
+print(C.Expr(Symbol("XX1")));
+print(C.Expr("XX2"));
+print(C.Expr(112));
+print(C.Expr([Symbol("XXF1")]));
+print(C.Expr([Symbol("XXF"),1,2,3]));
+print(C.Expr([Symbol("XX"),1,2,"xx","yy",Symbol("wut")]));
+print(C.Block([[Symbol("XX1"),1,"xxy"],
 		      [Symbol("XX2"),12,"xxz"],
 		      [Symbol("XX3"),44,"xxg"]]));
-print(C.compileFunc("eep",["a","b","c"],
-		    [[Symbol('print'),'x'],
-		     [Symbol('print'),'x'],
-		     [Symbol('print'),'x']]));
-print(C.compileSetq(Symbol("eep"),[Symbol("list"),"a","b","c"]));
+print(C.Func("eep",["a","b","c"],
+	     [Symbol('print'),'x'],
+	     [Symbol('print'),'x'],
+	     [Symbol('print'),'x']));
+print(C.Setq(Symbol("eep"),[Symbol("list"),"a","b","c"]));
